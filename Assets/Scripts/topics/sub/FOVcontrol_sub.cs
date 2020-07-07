2fce25945d5266b7ee9bb4243666977823dd7e32
josematez <josematez@uma.es>
2020-07-07 13:48:58 +0200

    Added FoV control

---
+++ Assets/Scripts/topics/sub/FOVcontrol_sub.cs
@@ -0,0 +1,27 @@
+﻿using ROSBridgeLib;
+using ROSBridgeLib.std_msgs;
+using SimpleJSON;
+using UnityEngine;
+
+public class FOVcontrol_sub : ROSBridgeSubscriber
+{
+    public new static string GetMessageTopic()
+    {
+        return "active_perception/FOVcontrol";
+    }
+
+    public new static string GetMessageType()
+    {
+        return "std_msgs/Float32";
+    }
+
+    public new static ROSBridgeMsg ParseMessage(JSONNode msg)
+    {
+        return new Float32Msg(msg);
+    }
+
+    public new static void CallBack(ROSBridgeMsg msg, string host)
+    {
+        Object.FindObjectOfType<CameraRGBDSensor>().ChangeFoV(((Float32Msg)msg).GetData());
+    }
+}
